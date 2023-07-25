47675ba418afecd2a2a463c03ad60ba8f702822d
xiunianjun <1776527992@qq.com>
2023-07-25 13:47:14 +0800

    feat: refine basic logic of fifo scheduler

--- scheduler/fifo/fifo_lord.h
+++ scheduler/fifo/fifo_lord.h
@@ -2,35 +2,47 @@
 #include "lord.h"
 
 struct FifoTask {
-public:
-    FifoTask (int pid) : pid_(pid) {}
-
-private:
-    int pid_;
+    FifoTask (int pid) : pid(pid) {}
+    int pid;
 };
 
 class FifoRq {
 public:
     FifoRq() : rq_() {}
 
-    void enqueue(FifoTask task) {
-
+    void enqueue(u_int32_t tid) {
+        auto t = new FifoTask(tid);
+        rq_.push(t);
     }
 
     void dequeue() {
-
+        if (rq_.empty())    return ;
+        // remember to delete
+        auto t = rq_.front();
+        rq_.pop();
+        delete t;
     }
 
-    FifoTask peek() {
-
+    FifoTask* peek() {
+        if (rq_.empty())    return nullptr;
+        return rq_.front();
     }
 
     void remove_from_rq(u_int32_t tid) {
-
+        // remember to delete
+        for (auto it = rq_.begin(); it != rq_.end();) {
+            if ((*it) && (*it)->pid == tid) {
+                delete *it;
+                it = rq_->second.erase(it);
+                return;
+            } else {
+                it++;
+            }
+        }
     }
 
 private:
-    std::queue<FifoTask> rq_;
+    std::queue<FifoTask*> rq_;
 };
 
 class FifoLord : Lord {
@@ -40,7 +52,16 @@ public:
 
     virtual void schedule() {
         FifoTask* next = fifo_rq_.peek();
+        
+        // do shoot
+        int cpu = 7;
+        cpu_set_t mask;  
+        CPU_ZERO(&mask);    
+        CPU_SET(cpu, &mask);  
+        sa->area[cpu].pid = next->pid;
+        shoot_task(sizeof(mask), &mask);
 
+        fifo_rq_.dequeue();
     }
 
 private:
@@ -104,15 +125,17 @@ private:
     virtual void consume_msg_task_preempt(cos_msg msg) {
         u_int32_t tid = msg.pid;
         if (!alive_tasks_.count(tid)) {
-            LOG(ERROR) << "task dead before task new, kernel BUGGGGGG!";
+            LOG(ERROR) << "task preempt before task new, kernel BUGGGGGG!";
             exit(1);
         }
         
         // TODO
         if(task is queued) {
-            fifo_rq_.remove_from_rq(tid);
+            // We could be kQueued from a TASK_NEW that was immediately preempted.
+            // For fifo, we do nothing.
         } else if (task is oncpu) {
             update cpu state
+            fifo_rq_.enqueue(tid);
         }
     }
 
